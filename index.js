5177c19810802692efaea6a991d98661bfbc5153
Roystone-Were <patorankingquavo100@gmail.com>
2022-03-09 12:29:28 +0300

    second Commit

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
+
+// first player
 const randomNumber1 = Math.floor(Math.random()*6)+1;
-const randomDiceImage = `images/dice${randomNumber1}.png`; //includes all immages from dice1.png to dice6.png
+const randomDiceImage1 = `images/dice${randomNumber1}.png`; //includes all immages from dice1.png to dice6.png
 
 const image1 = document.querySelectorAll("img")[0]; //selects first <img> element which is [0]
 
-image1.setAttribute("src",randomDiceImage); //changes attribute when run
+image1.setAttribute("src",randomDiceImage1); //changes attribute when run
+
+//second player
+const randomNumber2 = Math.floor(Math.random()*6)+1;
+const randomDiceImage2 = `images/dice${randomNumber2}.png`;
+
+const image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomDiceImage2);
+
+
+//changing h1 element text
+const heading = document.querySelectorAll("h1")[0];
+
+if(randomNumber1 > randomNumber2){
+    heading.innerHTML= "player 1 won🎉";
+}else if(randomNumber2 > randomNumber1){
+    heading.innerHTML= "player 2 won🎉";
+}else{heading.innerHTML= "DRAW"};
+
+
+//refresh button
+const btn = document.querySelector("button");
+
+function reloading(){
+    window.location.reload()
+}
+btn.addEventListener("click",reloading);
+
+
